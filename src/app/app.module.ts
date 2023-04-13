5c87ab48c6c4277d8327016beca73d3d9f15f13e
alessandrobellotta <alessandro.bellotta@mail.polimi.it>
2023-04-13 16:02:15 +0200

    property binding example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
 import { SecondComponent } from './second/second.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     SecondComponent,
+    PropertyBindingComponent,
   ],
   imports: [
     BrowserModule
