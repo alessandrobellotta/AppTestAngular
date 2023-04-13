99b16be009a5f389fcc84072e58a5b74522ac18b
alessandrobellotta <alessandro.bellotta@mail.polimi.it>
2023-04-13 15:42:37 +0200

    create first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FirstComponentComponent,
+    SecondComponent,
   ],
   imports: [
     BrowserModule
